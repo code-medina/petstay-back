08409b749f7564d9013717be60614983c80e817c
gisele <medina.g.contact@gmail.com>
2026-04-27 21:31:48 -0300

    feat: add pino logger basic

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+//modules
 import express from 'express';
+//custom modules
+import { logger } from './lib/logger.js';
+
 const app = express();
-app.listen(8888);
+
+app.listen(8888, () => {
+  logger.info('server run on port 8888');
+});
