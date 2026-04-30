481aaef20521d41be814da1b1598ed321d050296
gisele <medina.g.contact@gmail.com>
2026-04-30 20:31:25 -0300

    add mongoose connection

---
+++ src/db/connection.ts
@@ -0,0 +1,20 @@
+import mongoose, { Mongoose } from 'mongoose';
+import { logger } from '../lib/logger.js';
+let connection: null | Mongoose = null;
+
+export const connectionDB = async () => {
+  const uri = process.env.URL_DB || '';
+  if (connection) disconnectDB();
+  try {
+    connection = await mongoose.connect(uri);
+    logger.info('successfull connection');
+  } catch (error) {
+    logger.error(error);
+
+    throw new Error('Error connection db', { cause: error });
+  }
+};
+export const disconnectDB = () => {
+  logger.info('disconnecting...');
+  if (connection) connection.disconnect();
+};
