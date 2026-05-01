956c666695856bbe6593070dd685db1730d5d5b8
gisele <medina.g.contact@gmail.com>
2026-04-30 21:11:16 -0300

    feat: add user schema

---
+++ src/models/user.schema.ts
@@ -0,0 +1,39 @@
+import { Schema, model, Types } from 'mongoose';
+
+export interface IUser {
+  _id: Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  role: 'tenant' | 'landlord';
+  favorites: string[];
+}
+const UserSchema = new Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+      maxLength: [30, 'name must be less than 30 characters'],
+    },
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      maxLength: [40, 'email must be less than 40 characters'],
+      unique: [true, 'email already exists'],
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+      maxLength: [50, 'password 50 must be less than 50 characters'],
+      minLength: [6, 'password must be at least 6 characters long'],
+    },
+    role: {
+      type: String,
+      enum: ['tenant', 'landlord'],
+      default: 'tenant',
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = model<IUser>('User', UserSchema);
