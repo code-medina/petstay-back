10219d55aa0d7649d9cda1884a4065fcc5189afd
gisele <medina.g.contact@gmail.com>
2026-05-06 22:05:20 -0300

    feat : add exist user

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,6 @@
-import type { CreateUserDtoType } from '../dto/user.dto.js';
-import { toHashPassword } from '../lib/hash.js';
+import type { CreateUserDtoType, LoginUserDtoTYpe } from '../dto/user.dto.js';
+import { checkPassword, toHashPassword } from '../lib/hash.js';
+import { generateRefreshToken, generateToken } from '../lib/jwt.js';
 import { logger } from '../lib/logger.js';
 import type { IAuthRepository } from '../repositiries/auth.repository.js';
 
@@ -10,6 +11,11 @@ export class AuthService {
   }
   registerUser = async (user: CreateUserDtoType) => {
     const { password, ...rest } = user;
+    const existUser = await this.repo.findByEmail({
+      email: rest.email,
+      password,
+    });
+    if (existUser) throw new Error('User already registered');
     const hashPassword = await toHashPassword(password);
 
     const data = { ...rest, password: hashPassword };
@@ -17,5 +23,17 @@ export class AuthService {
     logger.info(data);
 
     return this.repo.save(data);
+  };
+  loginUser = async (user: LoginUserDtoTYpe) => {
+    const userRegister = await this.repo.findByEmail(user);
+    if (!userRegister) throw new Error(' User Not Found ');
+
+    const isValid = await checkPassword(user.password, userRegister.password);
+    if (!isValid) throw new Error('Invalid credential');
+
+    //jsonwebtoken
+    const accessToken = generateToken(userRegister);
+    const refreshToken = generateRefreshToken(userRegister);
+    return { user: userRegister, refreshToken, accessToken };
   };
 }
