03c1e873082c659974a3cb607eb63109e3fab2ea
gisele <medina.g.contact@gmail.com>
2026-05-14 22:10:18 -0300

    feat: create service estate

---
+++ src/services/estate.service.ts
@@ -0,0 +1,21 @@
+import type { CreateEstateDtoType } from '../dtos/estate.dto.js';
+import { AppError } from '../errors/app.error.js';
+import { logger } from '../lib/logger.js';
+import type { IEstateRepository } from '../repositiries/estate.repository.js';
+
+export class EstateService {
+  private repo: IEstateRepository;
+  constructor(repository: IEstateRepository) {
+    this.repo = repository;
+  }
+
+  createEstate = (dto: CreateEstateDtoType) => {
+    logger.info('create estate service');
+    try {
+      return this.repo.save(dto);
+    } catch (error) {
+      logger.error(error);
+      throw new AppError('Failed create estate', 400);
+    }
+  };
+}
