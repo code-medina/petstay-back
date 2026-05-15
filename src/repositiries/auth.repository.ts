b157b162b12f219d35b24da5540853605f00e50a
gisele <medina.g.contact@gmail.com>
2026-05-14 21:45:17 -0300

    feat: add zod create estate dto

--- src/repositiries/auth.repository.ts
+++ src/repositiries/auth.repository.ts
@@ -1,22 +1,22 @@
 import { User, type IUser } from '../models/user.model.js';
-import type { CreateUserDtoType} from '../dto/user.dto.js';
+import type { CreateUserDtoType } from '../dtos/user.dto.js';
 
 import { logger } from '../lib/logger.js';
 
 export interface IAuthRepository {
   save(user: CreateUserDtoType): Promise<IUser>;
-  findByEmail(email:string): Promise<IUser | null>;
+  findByEmail(email: string): Promise<IUser | null>;
 }
 export class AuthRepository implements IAuthRepository {
   async save(user: CreateUserDtoType): Promise<IUser> {
     logger.info(user);
     return await User.create(user);
   }
-  async findByEmail(email:string): Promise<IUser | null> {
+  async findByEmail(email: string): Promise<IUser | null> {
     logger.info('find by email user');
     logger.info(email);
-    const userFind = await User.findOne({ email:email });
+    const userFind = await User.findOne({ email: email });
     logger.info(userFind);
-    return await User.findOne({ email:email } ).lean();
+    return await User.findOne({ email: email }).lean();
   }
 }
